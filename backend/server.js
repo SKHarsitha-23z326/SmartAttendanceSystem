1d18e7c630edd76fbd96dcc0f7c596c25c53c50f
unknown <harsithask@gmail.com>
2026-05-23 10:13:09 +0530

    Configure backend project structure for serverless Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -25,3 +25,4 @@ app.use('/api/attendance', attendanceRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
